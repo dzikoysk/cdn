779be5f3d5bd9bc2f9b11a070fc9a66dedaa826c
dzikoysk <dzikoysk@dzikoysk.net>
2020-07-24 00:57:15 +0200

    Bump Groovy to 3.0.5

--- src/main/java/net/dzikoysk/cdn/entity/Descriptions.java
+++ src/main/java/net/dzikoysk/cdn/entity/Descriptions.java
@@ -1,16 +1,14 @@
 package net.dzikoysk.cdn.entity;
 
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
-@Repeatable(Descriptions.class)
-public @interface Description {
+public @interface Descriptions {
 
-    String[] value() default {};
+    Description[] value();
 
 }
