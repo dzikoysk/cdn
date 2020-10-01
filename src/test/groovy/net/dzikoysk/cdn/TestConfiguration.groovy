392d8fb8172d969a248f13bd2f7bcfccbbc0891b
dzikoysk <dzikoysk@dzikoysk.net>
2020-10-01 16:14:24 +0200

    GH-17 Check unsupported fields (Resolve #17)

--- src/test/groovy/net/dzikoysk/cdn/TestConfiguration.groovy
+++ src/test/groovy/net/dzikoysk/cdn/TestConfiguration.groovy
@@ -17,10 +17,24 @@ class TestConfiguration {
         @Description('# Random value')
         public Integer subEntry = -1
 
-
         @Description('# List description')
         public List<String> list = [ 'record', 'record : with : semicolons' ]
 
+        // @Description('# Custom object')
+        //public CustomObject custom = new CustomObject('rtx', 3070)
+
+    }
+
+}
+
+class CustomObject {
+
+    private String id
+    private int count
+
+    CustomObject (String id, int count) {
+        this.id = id
+        this.count = count
     }
 
 }
